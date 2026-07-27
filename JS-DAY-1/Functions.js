5fdb48853f897eb0884a922171b1b212a877c6dc
Narinder Dev <narinder.dev@gmail.com>
2026-07-27 15:54:29 +0530

    secure bank account program finished

--- JS-DAY-1/Functions.js
+++ JS-DAY-1/Functions.js
@@ -1,14 +1,32 @@
 // The secure Bank Account
-function createdBankAccount(ammount,credit,debit){
+function createdBankAccount(ammount){
 let options = 
     {
-        deposit : ammount+=credit,
-        withdraw :debit,
-        getBalance : ammount-=debit,
+        deposit : function deposit(credit){
+            let depositAmmount = credit;
+            if(!depositAmmount<=0)
+             {let NewBalance = ammount+=credit;
+             console.log(`Deposit ammount = ${credit} Available balance = ${NewBalance}`);}
+            else{
+                console.log(`Deposit ammount should be greater than zero`)
+            }
+            },
+            withdraw : function deposit(debit){
+             let withdrawAmmount = debit
+             if(withdrawAmmount<ammount){
+                 let NewBalance = ammount-=debit; 
+                console.log(`Withdrwa ammount = ${debit} Available balance = ${NewBalance}`);
+            }else{
+                console.log(`Insufficient Balance`);
+            }},
+            getbalance : function getbalance(){
+                console.log(`Available balance ${ammount}`);
+            }
 }
-console.log("Deposit:",options.deposit)
-console.log("Withdraw:",options.withdraw)
-console.log("Balance:",options.getBalance)
+return options;
 }
-console.log(createdBankAccount(100,50,20));
+const myAccount = createdBankAccount(100);
+myAccount.deposit(100);
+myAccount.withdraw(20);
+myAccount.getbalance();
 
