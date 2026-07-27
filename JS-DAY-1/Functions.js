9e9726d7591bf9053c8b225b1a52b28ece4b71fb
Narinder Dev <narinder.dev@gmail.com>
2026-07-27 14:29:51 +0530

    working with functions

--- JS-DAY-1/Functions.js
+++ JS-DAY-1/Functions.js
@@ -1,1 +1,14 @@
 // The secure Bank Account
+function createdBankAccount(ammount,credit,debit){
+let options = 
+    {
+        deposit : ammount+=credit,
+        withdraw :debit,
+        getBalance : ammount-=debit,
+}
+console.log("Deposit:",options.deposit)
+console.log("Withdraw:",options.withdraw)
+console.log("Balance:",options.getBalance)
+}
+console.log(createdBankAccount(100,50,20));
+
