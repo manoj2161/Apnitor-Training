3f1a966748110cace14328e09e7243cfbe906d18
Narinder Dev <narinder.dev@gmail.com>
2026-07-28 18:26:13 +0530

    day to finished

--- Employee-Management-System/script.js
+++ Employee-Management-System/script.js
@@ -1,80 +1,99 @@
+
 const employees = [
-{
-id:1,
-name:"Manoj",
-email:"manoj@gmail.com",
-department:"IT",
-salary:50000,
-experience:2,
-status:"Active"
-},
-{
-id:2,
-name:"Rahul",
-email:"rahul@gmail.com",
-department:"HR",
-salary:30000,
-experience:1,
-status:"Inactive"
-},
-{
-id:3,
-name:"Rahul kumar",
-email:"rahul23@gmail.com",
-department:"HR",
-salary:35000,
-experience:1,
-status:"Inactive"
-}
+    {
+        id:1,
+        name:"Manoj",
+        email:"manoj@gmail.com",
+        department:"IT",
+        salary:50000,
+        experience:2,
+        status:"Active"
+    },
+    {
+        id:2,
+        name:"Rahul",
+        email:"rahul@gmail.com",
+        department:"HR",
+        salary:30000,
+        experience:1,
+        status:"Inactive"
+    },
+    {
+        id:3,
+        name:"Rahul kumar",
+        email:"rahul23@gmail.com",
+        department:"HR",
+        salary:35000,
+        experience:1,
+        status:"Inactive"
+    }
 ];
 
 
 function createEmployee(
-name,
-email,
-department,
-salary,
-experience,
-employees){
-    const highestId = employees.reduce((acc,curr)=>{
-        if(acc>curr.id){
-            return acc;
-        }
-        else{
-            return curr.id;
-        }
-    },0)
-    const nextId = highestId+1;
-    const newEmployee = {
-        id:nextId,
-        name,
-        email,
-        department,
-        salary,
-        experience,
-        status:"Active"
+    name,
+    email,
+    department,
+    salary,
+    experience,
+    employees){
+        const highestId = employees.reduce((acc,curr)=>{
+            if(acc>curr.id){
+                return acc;
+            }
+            else{
+                return curr.id;
+            }
+        },0)
+        const nextId = highestId+1;
+        const newEmployee = {
+            id:nextId,
+            name,
+            email,
+            department,
+            salary,
+            experience,
+            status:"Active"
+        };
+        return newEmployee;
+    }
+    const newEmployee = createEmployee("Karan","karan@gmail.com","IT",20000,0,employees);
+    const updatedEmployees = [...employees,newEmployee]
+    
+    
+    function renderEmployees(employees){
+        const employeeHTML = employees.map(employee=>{
+            return `<div class="employee">
+            <p>ID : ${employee.id}</p>
+            <p>NAME : ${employee.name}</p>
+            <p>EMAIL : ${employee.email}</p>
+            <p>DEPARTMENT : ${employee.department}</p>
+            <p>SALARY : ${employee.salary}</p>
+            <p>EXPERIENCE : ${employee.experience}</p>
+            <p>STATuS : ${employee.status}</p>
+            <br>
+            <button>Delete</button><button>Edit</button>
+            </div>
+            `
+        }).join("");
+        return employeeHTML;
     };
-    return newEmployee;
-}
-const newEmployee = createEmployee("Karan","karan@gmail.com","IT",20000,0,employees);
-const updatedEmployees = [...employees,newEmployee]
-console.log(updatedEmployees);
+    
+const myForm = document.querySelector("#myForm");
+const showEmployee = document.querySelector("#employeeList");
 
-const employeeList = document.querySelector("#employee-list");
-function renderEmployees(employees){
-    const employeeHTML = employees.map(employee=>{
-        return `<div>
-        <p>${employee.id}</p>
-        <p>${employee.name}</p>
-        <p>${employee.email}</p>
-        <p>${employee.department}</p>
-        <p>${employee.salary}</p>
-        <p>${employee.experience}</p>
-        <p>${employee.status}</p>
-        <button>Delete</button><button>Edit</button>
-        </div>
-        `
-    }).join("");
-    return employeeHTML;
-};
-employeeList.innerHTML=renderEmployees(employees);
+myForm.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const salary = document.querySelector("#salary").value;
+    const department = document.querySelector('input[name="department"]:checked').value;
+    const experience = document.querySelector("#experience").value;
+    console.log(name);
+    console.log(email);
+    console.log(salary);
+    console.log(experience);
+    console.log(department);
+});    
+    
+showEmployee.innerHTML=renderEmployees(updatedEmployees);
