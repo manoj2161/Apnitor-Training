892d1b87921daaad172f7219adf46d10dcc85aa2
manu2161 <thakurmanu065@gmail.com>
2026-08-03 17:15:45 +0530

    redndring keys exercise completed

---
+++ REACT-DAY-1/rendring-list-keys-in-react/src/components/PlantCard.jsx
@@ -0,0 +1,8 @@
+function PlantCard({name,waterEveryDays}){
+    return (<>
+
+        <p >Name : {name}</p>
+        <p>Watering Frequency : {waterEveryDays}</p>
+    </>)
+}
+export default PlantCard
