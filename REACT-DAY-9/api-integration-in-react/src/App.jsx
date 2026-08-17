6d0cf09c4c544cf96eb1d3bd6a4e4c0780125b54
manoj2161 <thakurmanu065@gmail.com>
2026-08-17 18:06:45 +0530

    creating own api then fetching the api in react solving cors problem also

--- REACT-DAY-9/api-integration-in-react/src/App.jsx
+++ REACT-DAY-9/api-integration-in-react/src/App.jsx
@@ -1,11 +1,27 @@
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-
+  const [data, setData] = useState([]);
+  const url = "http://localhost:3000/employees";
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(url);
+      const data = await response.json();
+      setData(data);
+      console.log(data);
+    }
+    getData();
+  }, []);
   return (
     <>
-      
+      {data.map((employee) => (
+        <div key={employee.id}>
+          <p>{employee.name}</p>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- REACT-DAY-9/employee-api/server.js
+++ REACT-DAY-9/employee-api/server.js
@@ -1,6 +1,8 @@
-import express from "express";
-const app = express();
-const port = 3000;
+import express from "express"; // importing express to use its features
+import cors from "cors";
+const app = express(); // initializing the server with the name of app
+app.use(cors());
+const port = 3000; // assigning the api server a port , in this case it is 3000
 const employees = [
   {
     id: 1,
@@ -17,22 +19,32 @@ const employees = [
     name: "Priya",
     role: "Tester",
   },
-];
+]; // data of employees in array object format
 app.get("/", (req, res) => {
-  res.send("Employee api is running");
+  res.send("Employee api is running"); // send the request to the user when the path or url is https://localhost:3000/
 });
 app.get("/employees", (req, res) => {
-  res.json(employees);
+  res.json(employees); //sends the request to the user when the path or url is https://localhost:3000/employees
 });
 app.get("/employees/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const employee = employees.find((employee) => employee.id === id);
+  const id = Number(req.params.id); // if we need to send a particular data then we use :id parameter:id  , the req is given in string format we need to convert it into number first
+  const employee = employees.find((employee) => employee.id === id); // then we find the desired response
   if (employee) {
-    return res.json(employee);
+    return res.json(employee); //if responce found then we send the response
   } else {
-   res.status(404).send("Employee not found");
+    res.status(404).send("Employee not found"); // if response failed then we send error message with status 404
   }
 });
 app.listen(port, () => {
+  //it starts the server to its initial url like https://localhost:3000
   console.log(`server is running at port ${port}`);
 });
+
+//staus code i need to rember
+// code         meaning                 simple meaning
+// 200          ok                      everything worked
+// 201          Created                 something was successfully created
+// 400          Bad request             You sent something wrong
+// 401          Unauthorized            You are not authenticated/authorized
+// 404          Not found               The requested thihng does not exist
+// 500          internal server error   something went wrong on the server
