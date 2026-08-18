29d9e3cd4483d1ca0ee359afd05bad9e32487ef4
manoj2161 <thakurmanu065@gmail.com>
2026-08-18 11:04:40 +0530

    learned post method in both fronend and bakcedn

--- REACT-DAY-9/api-integration-in-react/src/App.jsx
+++ REACT-DAY-9/api-integration-in-react/src/App.jsx
@@ -3,23 +3,77 @@ import { useState } from "react";
 
 function App() {
   const [data, setData] = useState([]);
+  const [name, setName] = useState("");
+  const [role, setRole] = useState("");
+  const [error, setError] = useState("");
   const url = "http://localhost:3000/employees";
   useEffect(() => {
-    async function getData() {
-      const response = await fetch(url);
-      const data = await response.json();
-      setData(data);
-      console.log(data);
+    async function getEmployees() {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          return setError("API request failed");
+        }
+        const employees = await response.json();
+        setData(employees);
+        setError("");
+      } catch (error) {
+        setError(error.message);
+      }
     }
-    getData();
+    getEmployees();
   }, []);
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newEmployee = { name, role };
+    async function addEmployee() {
+      try {
+        const response = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newEmployee),
+        });
+        if (!response.ok) {
+          return setError("Failed to send data");
+        }
+        const addedEmployee = await response.json();
+        setData([...data, addedEmployee]);
+        setName("");
+        setRole("");
+        setError("")
+      } catch (error) {
+        setError(error.message);
+      }
+    }
+    addEmployee();
+  }
   return (
     <>
-      {data.map((employee) => (
-        <div key={employee.id}>
-          <p>{employee.name}</p>
-        </div>
-      ))}
+      {error && <p>{error}</p>}
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          id=""
+        />{" "}
+        <input
+          type="text"
+          name="role"
+          value={role}
+          onChange={(e) => setRole(e.target.value)}
+          id=""
+        />
+        <button type="submit">Add Employee</button>
+      </form>
+      <div>
+        {data.map((employee) => (
+          <p key={employee.id}>{employee.name}</p>
+        ))}
+      </div>
     </>
   );
 }
--- REACT-DAY-9/employee-api/server.js
+++ REACT-DAY-9/employee-api/server.js
@@ -1,50 +1,28 @@
-import express from "express"; // importing express to use its features
+import express from "express";
 import cors from "cors";
-const app = express(); // initializing the server with the name of app
+const app = express();
+const port = 3000;
 app.use(cors());
-const port = 3000; // assigning the api server a port , in this case it is 3000
+app.use(express.json());
+
 const employees = [
-  {
-    id: 1,
-    name: "Manoj",
-    role: "Developer",
-  },
-  {
-    id: 2,
-    name: "Rahul",
-    role: "Designer",
-  },
-  {
-    id: 3,
-    name: "Priya",
-    role: "Tester",
-  },
-]; // data of employees in array object format
-app.get("/", (req, res) => {
-  res.send("Employee api is running"); // send the request to the user when the path or url is https://localhost:3000/
-});
+  { id: 1, name: "Manoj", role: "frontend" },
+  { id: 2, name: "Aman", role: "PHP" },
+  { id: 3, name: "Karan", role: "Content Creator" },
+];
 app.get("/employees", (req, res) => {
-  res.json(employees); //sends the request to the user when the path or url is https://localhost:3000/employees
-});
-app.get("/employees/:id", (req, res) => {
-  const id = Number(req.params.id); // if we need to send a particular data then we use :id parameter:id  , the req is given in string format we need to convert it into number first
-  const employee = employees.find((employee) => employee.id === id); // then we find the desired response
-  if (employee) {
-    return res.json(employee); //if responce found then we send the response
-  } else {
-    res.status(404).send("Employee not found"); // if response failed then we send error message with status 404
-  }
+  res.json(employees);
 });
 app.listen(port, () => {
-  //it starts the server to its initial url like https://localhost:3000
-  console.log(`server is running at port ${port}`);
+  console.log("server is running at port", port);
 });
-
-//staus code i need to rember
-// code         meaning                 simple meaning
-// 200          ok                      everything worked
-// 201          Created                 something was successfully created
-// 400          Bad request             You sent something wrong
-// 401          Unauthorized            You are not authenticated/authorized
-// 404          Not found               The requested thihng does not exist
-// 500          internal server error   something went wrong on the server
+app.post("/employees", (req, res) => {
+  const id = employees.length + 1;
+  const newEmployee = {
+    id: id,
+    name: req.body.name,
+    role: req.body.role,
+  };
+  employees.push(newEmployee);
+  res.status(201).json(newEmployee);
+});
