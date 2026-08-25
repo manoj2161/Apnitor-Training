030d50abc63c1f0865044af81758b94121e39a8a
manoj2161 <thakurmanu065@gmail.com>
2026-08-25 18:24:28 +0530

    dark mode fixed

--- REACT-DAY-13/auth-aware-shopping-app-shell/src/components/NavBar.jsx
+++ REACT-DAY-13/auth-aware-shopping-app-shell/src/components/NavBar.jsx
@@ -9,33 +9,30 @@ export const NavBar = () => {
   console.log(theme);
   return (
     <>
-      <header
-        className={clsx(
-          "flex justify-between p-2 items-center h-16 text-black bg-white text-black",
-          theme && "dark:bg-gray-900 dark:text-white",
-        )}
-      >
-        <div className="text-xl font-semibold">
-          Hi, <span className="text-blue-600">{name}</span>
-        </div>
-        <div>
-          <button
-            onClick={() => {
-              theme ? setTheme(false) : setTheme(true);
-            }}
-          >
-            {theme ? <Sun /> : <Moon />}
-          </button>
-        </div>
-        <div>
-          <button
-            className="shadow-lg shadow-gray-700 rounded-lg p-1 text-gray-600"
-            onClick={() => {
-              setIsLoggedIn(false);
-            }}
-          >
-            {islogin ? "Log Out" : "Log In"}
-          </button>
+      <header className={theme ? "dark" : ""} c>
+        <div className="flex justify-between p-2 items-center h-16 text-black bg-white text-black dark:bg-gray-900 dark:text-white">
+          <div className="text-xl font-semibold">
+            Hi, <span className="text-blue-600">{name}</span>
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                theme ? setTheme(false) : setTheme(true);
+              }}
+            >
+              {theme ? <Sun /> : <Moon />}
+            </button>
+          </div>
+          <div>
+            <button
+              className="shadow-lg shadow-gray-700 rounded-lg p-1 text-gray-600"
+              onClick={() => {
+                setIsLoggedIn(false);
+              }}
+            >
+              {islogin ? "Log Out" : "Log In"}
+            </button>
+          </div>
         </div>
       </header>
     </>
