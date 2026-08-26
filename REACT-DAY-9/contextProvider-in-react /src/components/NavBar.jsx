2f832e60ee59d5ae1e5fe899d2dd59c2abf73545
Manoj Kumar <thakurmanu065@gmail.com>
2026-08-26 12:57:50 +0530

    Delete REACT-DAY-9/contextProvider-in-react  directory

--- REACT-DAY-9/contextProvider-in-react /src/components/NavBar.jsx
+++
@@ -1,9 +0,0 @@
- import { UserMenu } from "./UserMenu";
-export const NavBar = () => {
-  return (
-    <>
-      <h3>Usermenu</h3>
-      <UserMenu></UserMenu>
-    </>
-  );
-}
--- REACT-DAY-9/contextProvider-in-react /src/components/UserMenu.jsx
+++
@@ -1,9 +0,0 @@
-import { Avatar } from "./Avatar";
-export const UserMenu = () => {
-  return (
-    <>
-      <h4>Avatar</h4>
-      <Avatar></Avatar>
-    </>
-  );
-};
