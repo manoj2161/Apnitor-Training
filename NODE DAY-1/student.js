6bd8af946723ed78acdaea316bb18bcea0b99430
manoj2161 <thakurmanu065@gamil.com>
2026-09-13 18:52:16 +0530

    started learning node backend

---
+++ NODE DAY-1/student.js
@@ -0,0 +1,6 @@
+const name = process.argv[2];
+const age = process.argv[3];
+const course = process.argv[4];
+console.log(`Name : ${name} Age : ${age} Course : ${course}`);
+
+console.log(process.argv);
---
+++ NODE DAY-1/student1.ts
@@ -0,0 +1,7 @@
+const name: string = process.argv[2];
+const age: number = Number(process.argv[3]);
+const course: string = process.argv[4];
+console.log(`Name : ${name}`);
+console.log(`Age : ${age}`);
+console.log(`Course : ${course}`);
+console.log(process.argv);
